9835f5548bfce0066cb479afb48e0c55a79edc52
ali-zargari <77089778+ali-zargari@users.noreply.github.com>
2021-11-16 19:37:53 -0800

    Game breaking bugs fixed. Player can now successfully choose a shape.
    program is more efficient

--- js/game_controller.js
+++ js/game_controller.js
@@ -14,6 +14,7 @@ const Gameboard =  (() => {
     let opponent_shape = '';
     let opponent_name = 'opponent'
     let cells = [''*9]; // array of cells
+    let simplified_cells = ['-'*9];
     let grid = ''; //grid to fill with cells.
     let p_score = 0;
     let o_score = 0;
@@ -33,9 +34,9 @@ const Gameboard =  (() => {
             cells[i]= new Cell(i, false);
             cells[i].placeInGrid(grid);
             let temp = Tools.addElement('div',"content", cells[i].getCellElement())
-            Tools.addElement('i',"X fas fa-times", temp, undefined, '');
+            Tools.addElement('i',"X fas fa-times", temp, undefined, 'X');
             Tools.addElement('i',"O far fa-circle", temp, undefined, 'O');
-            cells[i].resetCell();
+
         }
 
         /**
@@ -45,6 +46,9 @@ const Gameboard =  (() => {
         o_score = 0;
         let temp = document.getElementsByClassName('reset');
         temp.item(0).addEventListener('click', function (e){
+            cells.forEach(c => {
+                c.resetCell();
+            })
             init();
         })
 
@@ -56,10 +60,10 @@ const Gameboard =  (() => {
 
                if(e.target.className === 'itext P1'){
                    document.querySelector('.p_name').innerHTML = player_name;
-                   console.log(document.querySelector('#p_name').value);
+                   //console.log(document.querySelector('#p_name'));
                } else {
                    document.querySelector('.o_name').innerHTML = player_name;
-                   console.log(document.querySelector('#p_name').value);
+                   //console.log(document.querySelector('#p_name'));
                }
 
                //console.log(player_name)
@@ -70,17 +74,24 @@ const Gameboard =  (() => {
         temp.forEach(e => {
            e.addEventListener('click',function (e){
                let temp = e.currentTarget;
-               //change the button thats pressed
+               //activate board
+               cells.forEach(c =>{
+                   c.addELS();
+               })
+
+               //change the button that's pressed
                temp.style.backgroundImage = '-webkit-linear-gradient(top, #3cb0fd, #3498db)';
-               player_shape = temp;
+               player_shape = temp.classList[1];
+
                //change color of the other button
                if(temp.nextElementSibling == null){
                    temp.parentElement.firstElementChild.style.backgroundImage = '-webkit-linear-gradient(top, #3498db, #2980b9)';
-                   opponent_shape = temp.parentElement.firstElementChild;
-                   return;
+                   opponent_shape = temp.parentElement.firstElementChild.classList[1];
+
                } else{
+
                    temp.nextElementSibling.style.backgroundImage = '-webkit-linear-gradient(top, #3498db, #2980b9)';
-                   opponent_shape = temp.nextElementSibling;
+                   opponent_shape = temp.nextElementSibling.classList[1];
                }
 
 
@@ -101,18 +112,30 @@ const Gameboard =  (() => {
     /**
      * return the cell at index i
      *
-     * @param i index
-     * @returns Cell object
      */
     const getCells = () => {
         return cells;
     }
 
     /**
+     * return the player 1 shape
+     *
+     */
+    const getPlayerShape = () => {
+        return player_shape;
+    }
+
+    /**
+     * return the player 1 shape
+     *
+     */
+    const getOpponentShape = () => {
+        return opponent_shape;
+    }
+
+    /**
      * Draws X or O in given cell
      *
-     * @param shape X or O
-     * @param cell cell to place the shape at
      */
     const draw = (cell) =>{
         let cell_element = cell;
@@ -130,7 +153,7 @@ const Gameboard =  (() => {
 
     }
 
-    return {cells, getCellByID, init, draw, drawAtIndex, getCells};
+    return {cells, getCellByID, getPlayerShape, getOpponentShape, init, draw, drawAtIndex, getCells};
 })();
 
 /**
@@ -141,8 +164,11 @@ function Cell (id, played) {
     let elem = '';
     this.id = id;
     this.played = played;
+    let current_shape = '-';
 
-
+    this.getShape = function (){
+        return current_shape;
+    }
 
     /**
      * Add all needed event listeners to this cell.
@@ -150,7 +176,7 @@ function Cell (id, played) {
      *
      */
     this.addELS = function(){
-        elem.addEventListener('click', Controller.playRound);
+        elem.addEventListener('click', GameController.playRound);
         elem.addEventListener('mouseenter', this.appear);
         elem.addEventListener('mouseleave', this.disappear);
     }
@@ -169,7 +195,6 @@ function Cell (id, played) {
         elem = Tools.addElement('div', `cell c${id}`, grd, id);
         elem.style.gridArea = 'c'+(id);
         elem.style.width = '100%';
-        this.addELS();
     }
 
     /**
@@ -194,9 +219,12 @@ function Cell (id, played) {
      */
     this.disappear = function(all){
 
-        let temp_shape = 'O';
-        if(Controller.isPlayerTurn())
-            temp_shape = 'X';
+        let temp_shape = Gameboard.getOpponentShape().toUpperCase();
+        //let temp_shape = 'X';
+        if(GameController.isPlayerTurn()) {
+            temp_shape = Gameboard.getPlayerShape().toUpperCase();
+            //let temp_shape = 'O';
+        }
 
         window.animatelo.fadeOut(`.c${id} .content .${temp_shape}`);
 
@@ -207,10 +235,11 @@ function Cell (id, played) {
      */
     this.appear = function (){
 
-        let temp_shape = 'O';
-        if(Controller.isPlayerTurn())
-            temp_shape = 'X';
-
+        let temp_shape = Gameboard.getOpponentShape().toUpperCase();
+        if(GameController.isPlayerTurn()) {
+            temp_shape = Gameboard.getPlayerShape().toUpperCase();
+        }
+        //console.log(typeof temp_shape.toUpperCase() + typeof " X");
         window.animatelo.fadeIn(`.c${id} .content .${temp_shape}`);
 
     }
@@ -225,19 +254,26 @@ function Cell (id, played) {
 
         elem.removeEventListener('mouseleave', this.disappear);
         elem.removeEventListener('mouseenter', this.appear);
-        elem.removeEventListener('click', Controller.playRound);
+        elem.removeEventListener('click', GameController.playRound);
         this.played = true;
 
+        if(GameController.isPlayerTurn()) {
+            current_shape = Gameboard.getPlayerShape();
+            console.log(current_shape)
+        } else {
+            current_shape = Gameboard.getOpponentShape();
+            console.log(current_shape)
+        }
     }
 
 };
 
 
 /**
- * Controller object to control the flow of the game. Makes use of Cell and Gameboard Objects.
+ * Controller object to control the flow of the game. Makes use of Gameboard Object.
  * @type {{playRound: playRound, startGame: startGame, isPlayerTurn: (function(): boolean)}}
  */
-let Controller =  (() => {
+let GameController =  (() => {
     let player_turn = true;
     let selected_cell = 'NOT_SELECTED'
 
@@ -263,13 +299,15 @@ let Controller =  (() => {
     function playRound (e){
 
         let played_cell = Gameboard.getCellByID(e.srcElement.parentNode.parentNode.id);
-        //console.log(played_cell.id)
-        played_cell.lockState(e);
-        player_turn = !player_turn;
-        if(game_status === 'running'){
-            console.log("game is running");
+        //console.log(played_cell)
+        if(played_cell) {
+            played_cell.lockState(e);
+            player_turn = !player_turn;
         }
-        console.log(Gameboard.getCells());
+        //if(game_status === 'running'){
+         //   console.log("game is running");
+        //}
+        //console.log(Gameboard.getCells()[0]);
     }
 
 
@@ -280,4 +318,4 @@ let Controller =  (() => {
     return {startGame, playRound, isPlayerTurn};
 })();
 
-Controller.startGame();
+GameController.startGame();
